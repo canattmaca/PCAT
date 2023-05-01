13d035ec5c77094df26256b09cee982b994b692c
canattmaca <119670826+canattmaca@users.noreply.github.com>
2023-05-01 15:36:44 +0300

    static files uplouded

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const fn = function () {
-  console.log('funtion working');
-};
 //Save exact devDependies e atıyor
+//--save dependencies= zorunlu !
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
